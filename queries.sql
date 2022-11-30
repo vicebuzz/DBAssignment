d5776c7c644a09512d5d4779c54ac7e21634bbfe
vicebuzz <victor.lav4061@gmail.com>
2022-11-30 11:24:33 +0000

    structure_queries

---
+++ queries.sql
@@ -0,0 +1,10 @@
+--CREATE SCRIPTS
+/* put your create scripts here - your script should not be commented out */
+--INSERT SCRIPTS
+/* put your insert scripts here - your script should not be commented out */
+--SELECT SCRIPTS
+/* put your select scripts here (with indication of which query is answered by each) - your
+script should not be commented out */
+--DROP SCRIPTS
+/* put your drop scripts here (in the correct order) - your script should not be commented out
+*/
